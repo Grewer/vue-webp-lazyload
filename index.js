dcfdff76491cccd266b5ed287583a082c076948d
zhangyu <grewer@grewer.cn>
2019-11-12 23:43:51 +0800

    项目方案优化 (未完成

--- index.js
+++ index.js
@@ -9,6 +9,27 @@ const checkWebpSupport = () => {
   }
 }
 
+const throttle = function () {
+  var isClear = arguments[0], fn;
+  if (typeof isClear === 'boolean') {
+    fn = arguments[1];
+    fn.__throttleID && clearTimeout(fn.__throttleID)
+  } else {
+    fn = isClear;
+    var p = Object.assign({
+      context: null,
+      args: [],
+      time: 300,
+    }, arguments[1]);
+
+    throttle(true, fn);
+
+    fn.__throttleID = setTimeout(function () {
+      fn.apply(p.context, p.args)
+    }, p.time)
+  }
+}
+
 const loadNewImg = (src) => {
   return new Promise((resolve, reject) => {
     let img = new Image()
@@ -21,6 +42,7 @@ const loadNewImg = (src) => {
     img.src = src
   })
 }
+
 
 lazyLoad.install = function (Vue, options) {
   // loading 时的图片
@@ -35,140 +57,67 @@ lazyLoad.install = function (Vue, options) {
     openWebp = true
   } = options
 
-  let check = null
 
-  // openWebp need check
-
-
-  let store = {
-    queue: [],
-    topics: {},
-    push: function (value) {
-      this.queue.push(value)
-      this.topics['push'] && this.topics['push'](value);
-    },
-    addListener: function (topic, cb) {
-      this.topics[topic] = cb;
-    }
-  };
+  let store = []; // store images
 
   if (openWebp) {
-    check = checkWebpSupport()
+    isSupportWebp = checkWebpSupport()
   }
 
-  let replace = (i, isAllow) => {
-    // todo fix or remove
-    if (isAllow) {
-      replace = (i) => {
-        let {el, binding} = i
-        let webp = binding.value.replace(/(\.jpg|\.png)/g, ".webp")
-        loadNewImg(webp).then(() => {
-          el.src = webp;
-          imgCache[webp] = 1
-          i.status = 'loaded'
-        }).catch(() => {
-          el.src = failImg || ''
-        })
+  const load = (function () {
+    const getUrl = openWebp && isSupportWebp ? (binding) => {
+        return binding.value.webp || binding.value.image
+      } :
+      (binding) => {
+        return binding.value.image
       }
-      replace(i)
-    } else {
-      replace = (i) => {
-        let {el, binding} = i
-        let webp = binding.value
-        loadNewImg(webp).then(() => {
-          el.src = webp;
-          imgCache[webp] = 1
-          i.status = 'loaded'
-        }).catch(() => {
-          el.src = failImg || ''
-        })
-      }
-      replace(i)
+
+    return (img) => {
+      let {el, binding} = img
+      const url = getUrl(binding)
+      loadNewImg(url).then(() => {
+        el.src = url;
+      }).catch(() => {
+        el.src = failImg || ''
+      })
     }
-  }
+  })()
 
 
-
-
-  const checkInView = (el) => {
+  const shouldShow = (i) => {
+    const {el} = store[i];
     let {top, bottom, left, right} = el.getBoundingClientRect()
     return (top < window.innerHeight && bottom > 0) &&
       (left < window.innerWidth && right > 0)
   }
 
 
-  let asyncScroll = null
+  const update = () => {
+    if (!store.length) {
+      return;
+    }
 
-  const scroll = () => {
-    let queue = store.queue
-    if (queue.length > 0) {
-      for (let i = 0, l = queue.length; i < l; i++) {
-        if (queue[i]) {
-          if (queue[i].status === 'loaded') {
-            queue.splice(i, 1)
-            continue;
-          }
-          load(queue[i])
-        }
+    let i = store.length;
+
+    while (i--) {
+      if (shouldShow(i)) {
+        load(store[i])
+        store.splice(i, 1);
       }
     }
   }
 
-  window.onscroll = function () {
-    if (asyncScroll) {
-      clearTimeout(asyncScroll)
-    }
-    asyncScroll = setTimeout(scroll, 500)
-  };
-
-
-  let imgCache = {} //缓存已经加载过的图片
-
-  const load = (i) => {
-    let {el, binding} = i
-    let _load = () => {
-      if (openWebp) {
-        if (isSupportWebp === null) {
-          if(check){
-            _load = () => {
-              replace(i, isSupportWebp)
-            }
-            _load()
-          }
-        } else {
-          _load = () => {
-            replace(i, isSupportWebp)
-          }
-          _load()
-        }
-      } else {
-        _load = () => {
-          loadNewImg(binding.value).then(() => {
-            el.src = binding.value;
-            imgCache[binding.value] = 1
-            i.status = 'loaded'
-          }).catch(() => {
-            el.src = failImg || ''
-          })
-        }
-        _load()
-      }
-    }
-
-    Vue.nextTick(() => {
-      if (checkInView(el)) {
-        if (imgCache[binding.value]) {
-          el.src = binding.value
-        } else {
-          _load()
-        }
-      }
-    })
-
+  const bindEvent = () => {
+    window.addEventListener('resize',function () {
+      throttle(update, {context: this})
+    },false)
+    window.addEventListener('scroll',function () {
+      throttle(update, {context: this})
+    },false)
   }
 
-
-  store.addListener('push', load)
+  update();
+  bindEvent();
 
   Vue.directive('webp', {
     bind(el, binding) {
@@ -177,5 +126,7 @@ lazyLoad.install = function (Vue, options) {
     }
   })
 }
+
+// 项目开始时未触发 // 页面进入时
 
 export default lazyLoad
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,47 @@
 <template>
   <div class="hello">
-    <img v-webp="'https://grewer.github.io/dataSave/back.jpg'" >
-    <img v-webp="'https://grewer.github.io/dataSave/hero.jpg'" alt="">
-    <img v-webp="'https://grewer.github.io/dataSave/monster.jpg'" alt="" style="position: absolute;top: 2000px;left: 0">
-    <img v-webp="'https://grewer.github.io/dataSave/s.jpg'" alt="" style="position: absolute;top: 2000px;left: 100px;">
+    <img v-webp="{image:'https://grewer.github.io/dataSave/back.jpg',
+      webp:'https://grewer.github.io/dataSave/back.webp'}">
+    <img alt=""
+         v-webp="{image:'https://grewer.github.io/dataSave/hero.jpg',
+         webp:'https://grewer.github.io/dataSave/hero.webp'}">
+    <img alt="" style="position: absolute;top: 2000px;left: 0"
+         v-webp="{image:'https://grewer.github.io/dataSave/monster.jpg',
+         webp:'https://grewer.github.io/dataSave/monster.webp'}">
+    <img alt="" style="position: absolute;top: 2000px;left: 100px;"
+         v-webp="{image:'https://grewer.github.io/dataSave/s.jpg',
+         webp:'https://grewer.github.io/dataSave/s.webp'}">
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    export default {
+        name: 'HelloWorld',
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
     }
-  }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
 </style>
